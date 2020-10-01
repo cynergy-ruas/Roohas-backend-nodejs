dc3c9c63595bcf2284f74a2839610dd520e1f138
subhendu17620 <subhendu17620@gmail.com>
2020-10-01 16:19:59 +0530

    almost complete

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const fbAuth = require("./fbAuth");
-// const { login, userBasedFunc } = require("./user");
+const fbAuth = require("./fbAuth");
+const { login, userBasedFunc } = require("./user");
 
 const PORT = 3000;
 app.use(cors());
