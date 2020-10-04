8e17263e858463844ef6a2a8929b1ffe8f835060
subhendu17620 <subhendu17620@gmail.com>
2020-10-04 22:32:41 +0530

    correct code

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
-require("dotenv");
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const fbAuth = require("./fbAuth");
-const { login, userBasedFunc } = require("./user");
+const mongoose = require("mongoose");
 
-const PORT = 3000;
-app.use(cors());
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
+
+const { signup, login, verify } = require("./api/auth/user");
 
 app.get("/", (req, res) => {
-  res.send("Hey! This is working !");
+  res.send("hey, Its working");
 });
+app.post("/signup", signup);
 app.post("/login", login);
-app.get("/userBasedFunc", fbAuth, userBasedFunc);
 
+app.get("/verify", verify);
+
+const db_url =
+  "mongodb+srv://subhendu:subhendu@cluster0.qthyq.mongodb.net/roohas-test?retryWrites=true&w=majority";
+mongoose.connect(
+  db_url,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to mongoDB ..");
+  }
+);
+//Listen Server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`server running at port ${PORT}`);
+  console.log("Server running at port", PORT);
 });
